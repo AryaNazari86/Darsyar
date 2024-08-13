d9d56ba97a9a8c4c958d3ff2f516791f15458e62
AryaNazari86 <nazariarya86@gmail.com>
2024-08-13 02:48:17 -0700

    changes

--- Darsyar/settings.py
+++ Darsyar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&!5y8)=ivdo58hl#=v(87o^t1gtzy+6p-ppu0%c$o753!_9-)m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+'''class LOG(models.Model):
+
+    date_created = models.DateTimeField(auto_now=True)
+
+    TYPES = (
+        (0, 'Question'),
+        (1, 'Test'),
+        (2, 'AI'),
+    )
+
+    #type = models.IntegerField(default=0, choices=TYPES) '''
